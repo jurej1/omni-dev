563c8143d31414b519b097f4f3fdbe83ce0b8b44
Jure Jures <jure.04jj@gmail.com>
2026-03-12 15:30:05 +0100

    adding all available tools for now

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,3 +1,21 @@
+import { bashTool } from "./bash/bash";
+import { editTool } from "./edit/edit";
+import { globTool } from "./glob/glob";
+import { grepTool } from "./grep/grep";
+import { listTool } from "./ls/ls";
 import { readTool } from "./read/read";
+import { webfetchTool } from "./webfetch/webfetch";
+import { websearchTool } from "./websearch/websearch";
+import { writeTool } from "./write/write";
 
-export const tools = [readTool];
+export const tools = [
+  readTool,
+  bashTool,
+  editTool,
+  writeTool,
+  websearchTool,
+  webfetchTool,
+  listTool,
+  grepTool,
+  globTool,
+];
