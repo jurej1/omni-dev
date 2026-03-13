d5db1711c0a75e78b09163ffe89cdb362692bf9c
Jure Jures <jure.04jj@gmail.com>
2026-03-13 09:17:46 +0100

    adding textarea

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,19 +1,35 @@
-import { createSignal } from "solid-js";
 import { useOpenRouter } from "../context/openrouter";
+import { TextareaRenderable } from "@opentui/core";
+
+const PLACEHOLDERS = [
+  "Fix a TODO in the codebase",
+  "What is the tech stack of this project?",
+  "Fix broken tests",
+];
 
 export function Input() {
-  const [value, setValue] = createSignal("");
+  let input: TextareaRenderable;
 
   const { callModel } = useOpenRouter();
 
+  const submit = async () => {
+    const value = input.plainText;
+
+    callModel(value);
+    input.clear();
+  };
+
   return (
     <box borderStyle="single" flexShrink={1}>
-      <input
+      <textarea
         focused={true}
-        value={value()}
-        onInput={setValue}
-        onSubmit={callModel}
-      ></input>
+        placeholder={PLACEHOLDERS[0]}
+        minHeight={1}
+        maxHeight={6}
+        onSubmit={submit}
+        onMouseDown={(e) => e.target.focus()}
+        ref={input}
+      ></textarea>
     </box>
   );
 }
